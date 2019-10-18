6659e09e4c6682cfd1dff5434e49c2ae24e34897
Leo <leonardom1808@gmail.com>
2019-10-18 14:09:43 -0300

    Feito e testado

--- src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/APS.java
@@ -14,6 +14,7 @@ public class APS {
         model.add(new OrGate());
         model.add(new AndGate());
         model.add(new XorGate());
+        model.add(new TripleInputOrGate());
 
         View view = new View(model);
 
--- src/main/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputOrGate.java
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/TripleInputOrGate.java
@@ -1,4 +1,58 @@
 package br.pro.hashi.ensino.desagil.aps.model;
 
 public class TripleInputOrGate extends Gate {
+
+    private final NandGate nandTop;
+    private final NandGate nandMid1;
+    private final NandGate nandBottom;
+    private final NandGate nandFinal;
+
+
+    public TripleInputOrGate() {
+        super("TripleInputOR", 3);
+
+        nandTop = new NandGate();
+        nandMid1 = new NandGate();
+        nandBottom = new NandGate();
+        NandGate nandMid2 = new NandGate();
+        NandGate nandMid3 = new NandGate();
+        nandFinal = new NandGate();
+
+
+        nandMid2.connect(0, nandTop);
+        nandMid2.connect(1, nandMid1);
+
+        nandMid3.connect(0, nandMid2);
+        nandMid3.connect(1, nandMid2);
+
+        nandFinal.connect(0, nandMid3);
+        nandFinal.connect(1, nandBottom);
+
+    }
+
+    @Override
+    public boolean read(int outputPin) {
+        if (outputPin != 0) {
+            throw new IndexOutOfBoundsException(outputPin);
+        }
+        return nandFinal.read();
+    }
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+        switch (inputPin) {
+            case 0:
+                nandTop.connect(0, emitter);
+                nandTop.connect(1, emitter);
+                break;
+            case 1:
+                nandMid1.connect(0, emitter);
+                nandMid1.connect(1, emitter);
+
+            case 2:
+                nandBottom.connect(0, emitter);
+                nandBottom.connect(1, emitter);
+        }
+
+    }
 }
